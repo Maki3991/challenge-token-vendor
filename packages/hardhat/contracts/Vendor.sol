a7fe06b1ef00d73fab7d2d9e7c30125fc29dbe42
Yuxuan Qu <1344933709@qq.com>
2025-12-09 16:51:32 +0800

    checkpoint 2 save

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,40 @@
-pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
 
-    constructor(address tokenAddress) {
+    constructor(address tokenAddress) Ownable(msg.sender) {
         yourToken = YourToken(tokenAddress);
     }
 
-    // ToDo: create a payable buyTokens() function:
+    // 💰 买币函数
+    function buyTokens() public payable {
+        uint256 amountToBuy = msg.value * tokensPerEth;
 
-    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        // 建议加上：检查售货机有没有足够的货，虽然 transfer 也会报错，但这样报错信息更友好
+        uint256 vendorBalance = yourToken.balanceOf(address(this));
+        require(vendorBalance >= amountToBuy, "Vendor has insufficient tokens");
+
+        // 发送代币
+        bool sent = yourToken.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer token to user"); // 👈 关键修复
+
+        emit BuyTokens(msg.sender, msg.value, amountToBuy);
+    }
+
+    // 🏧 提款函数 (Checkpoint 2 要求)
+    function withdraw() public onlyOwner {
+        (bool sent, ) = msg.sender.call{ value: address(this).balance }("");
+        require(sent, "Failed to send Ether");
+    }
 
     // ToDo: create a sellTokens(uint256 _amount) function:
+    // (留给 Checkpoint 3 再写)
 }
